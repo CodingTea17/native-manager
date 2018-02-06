8d0a3766599f4ed26680735936560f0ef13a8673
Dawson M <dawson.mortenson@gmail.com>
2018-02-06 14:39:09 -0800

    setup firebase, install react-native-dotenv, and create an initial reducer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import { APIKEY, AUTHDOMAIN, DATABASEURL, PROJECTID, STORAGEBUCKET, MESSAGINGSENDERID } from 'react-native-dotenv'
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: APIKEY,
+      authDomain: AUTHDOMAIN,
+      databaseURL: DATABASEURL,
+      projectId: PROJECTID,
+      storageBucket: STORAGEBUCKET,
+      messagingSenderId: MESSAGINGSENDERID
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
         <View>
           <Text>
             Hello, Manager!
           </Text>
         </View>
-    </Provider>
     );
   }
 }

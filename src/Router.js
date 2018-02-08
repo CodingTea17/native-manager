f4b7789510a98682bff650907da7d830f65ef57a
Dawson M <dawson.mortenson@gmail.com>
2018-02-08 08:25:41 -0800

    fill out the employee create form component

--- src/Router.js
+++ src/Router.js
@@ -9,7 +9,12 @@ const RouterComponent = () => {
     <Router>
       <Scene key="root" hideNavBar>
         <Scene key="auth">
-          <Scene key="login" component={LoginForm} title="Please Login" initial />
+          <Scene
+            key="login"
+            component={LoginForm}
+            title="Please Login"
+            initial
+          />
         </Scene>
         <Scene key="main">
           <Scene
@@ -18,6 +23,7 @@ const RouterComponent = () => {
             key="employeeList"
             component={EmployeeList}
             title="Employees"
+            initial
           />
           <Scene
             key="employeeCreate"
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Add an employee?</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Bob"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+        
+        <CardSection>
+          <Button>
+            Add Employee
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 }

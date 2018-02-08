33215a135d270b6ee70b97707f4081143432c857
Dawson M <dawson.mortenson@gmail.com>
2018-02-08 09:52:59 -0800

    add a picker to the create employee form and minor refactors

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Picker } from 'react-native';
+import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common';
+import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
   render() {
+    const { name, phone, shift, employeeUpdate } = this.props;
+
     return (
       <Card>
         <CardSection>
           <Input
             label="Name"
             placeholder="Bob"
+            value={name}
+            onChangeText={(value) => employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
 
@@ -17,12 +23,27 @@ class EmployeeCreate extends Component {
           <Input
             label="Phone"
             placeholder="555-555-5555"
+            value={phone}
+            onChangeText={(value) => employeeUpdate({ prop: 'phone', value })}
           />
         </CardSection>
 
         <CardSection>
+          <Picker
+            style={{ flex: 1 }}
+            selectedValue={this.props.shift}
+            onValueCahnge={(value) => employeeUpdate({ prop: 'shift', value })}
+          >
+            <Picker.Item label="Monday" value="Monday"/>
+            <Picker.Item label="Tuesday" value="Tuesday"/>
+            <Picker.Item label="Wednesday" value="Wednesday"/>
+            <Picker.Item label="Thursday" value="Thursday"/>
+            <Picker.Item label="Friday" value="Friday"/>
+            <Picker.Item label="Saturday" value="Saturday"/>
+            <Picker.Item label="Sunday" value="Sunday"/>
+          </Picker>
         </CardSection>
-        
+
         <CardSection>
           <Button>
             Add Employee
@@ -33,4 +54,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) =>{
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -3,6 +3,7 @@ import { EMPLOYEE_UPDATE } from '../actions/types';
 const INITIAL_STATE = {
   name: '',
   phone: '',
+  shift: ''
 };
 
 export default (state = INITIAL_STATE, action) => {

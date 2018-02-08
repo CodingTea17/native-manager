5a3e2fd98892145c2603f89aaa28940314ee1e3a
Dawson M <dawson.mortenson@gmail.com>
2018-02-08 14:53:44 -0800

    reset form on successful employee creation

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -3,7 +3,6 @@ import { Actions } from 'react-native-router-flux';
 import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
-  console.log(value)
   return {
     type: EMPLOYEE_UPDATE,
     payload: { prop, value }
@@ -13,9 +12,12 @@ export const employeeUpdate = ({ prop, value }) => {
 export const employeeCreate = ({ name, phone, shift }) => {
   // Come back to this and make it load from state? Also why does this work without passing auth details?
   const { currentUser } = auth();
-  return () => {
+  return (dispatch) => {
     database().ref(`/users/${currentUser.uid}/employees`)
       .push({ name, phone, shift })
-      .then(() => Actions.pop());
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE })
+        Actions.pop();
+      });
   }
 }

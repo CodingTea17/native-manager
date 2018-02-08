5a79e996567933ff94b2cd56a4c10276a7edaa44
Dawson M <dawson.mortenson@gmail.com>
2018-02-08 08:32:27 -0800

    add employee actions and form reducer

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,11 @@
+import { EMPLOYEE_UPDATE } from '../actions/types';
+
+const INITIAL_STATE = {
+
+};
+
+export default (state = INITIAL_STATE, action ) => {
+  switch(aciton.type)
+    default:
+      return state;
+}
